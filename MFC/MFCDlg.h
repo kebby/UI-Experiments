4956e13d3a876fc69bf2b6013886df3abc788a80
Tammo Hinrichs <kb@kebby.org>
2021-01-28 22:57:43 +0100

    Added MFC test project

---
+++ MFC/MFCDlg.h
@@ -0,0 +1,37 @@
+
+// MFCDlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCDlg dialog
+class CMFCDlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+    CComboBox Testbox;
+    virtual void OnOK();
+};
